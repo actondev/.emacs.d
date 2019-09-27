3598005f94be20e21bebbaab9981839d234182ef
actonDev <chris.actondev@gmail.com>
2019-09-27 18:01:26 +0200

    init.el without the custom set variables

--- init.el
+++ init.el
@@ -1,19 +1,12 @@
+(defun relative-from-init (file)
+  "Returns a complete file path relative from the init file.
+  eg if loaded ~/.emacs.d/init.el and you pass config.org you get
+  ~/.emacs.d/config.org"
+  (format "%s%s" (file-name-directory user-init-file) file))
+
 (package-initialize)
-(org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("2878517f049b28342d7a360fd3f4b227086c4be8f8409f32e0f234d129cee925" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
- '(package-selected-packages
-   (quote
-    (key-chord doom-themes powerline monokai-theme sublime-themes rainbow-delimiters helm use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (relative-from-init "config.org")) ;; loading relative, instead of "~/.emacs.d/config.org")
+
+(setq custom-file (relative-from-init ".emacs-custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
