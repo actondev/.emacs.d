12b8a68d66bedb79663ebe594ee08fb5562ac4ae
actondev <chris.actondev@gmail.com>
2020-07-20 15:21:29 +0200

    some aod.s7 work

--- elisp/aod-s7.el
+++ elisp/aod-s7.el
@@ -25,11 +25,25 @@
 	       (message "No (ns ..) form found!"))
 	   (scheme-send-string "(set! *ns* #f)"))))
 
+(defun aod.s7/top-level-sexp ()
+  (save-excursion
+    (end-of-defun)
+    (let ((end (point)))
+      (beginning-of-defun)
+      (string-trim
+       (buffer-substring-no-properties (point) end)))))
+
 (defun aod.s7/send-definition ()
+  "Sends the top level sexp under point.
+Note: (scheme-send-definition) would send 2 things:
+1. The top level sexp with a new line at the end (cause of (end-of-defun))
+2. The '\n' string as well. (extra commint command)
+
+Here we just send ounce the trimmed top level sexp"
   (interactive)
   (aod.s7/switch-to-ns)
-  (scheme-send-definition)
-  )
+  (scheme-send-string
+   (aod.s7/top-level-sexp)))
 
 (defun aod.s7/send-last-sexp ()
   (interactive)
