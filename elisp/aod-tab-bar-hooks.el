230675d3a1dd5bc763c122158b362991e356e6c3
Christos Vagias <christos.vagias@holoplot.com>
2023-01-26 16:19:18 +0100

    cleanups & forgotten things

--- elisp/aod-tab-bar-hooks.el
+++ elisp/aod-tab-bar-hooks.el
@@ -1,6 +1,7 @@
 ;;; aod-tab-bar-hooks.el --- Collection of hooks to show certain buffers in their own tab  -*- lexical-binding: t; -*-
 
-(defun aod.tab-bar/make-project-tab-bar-hook (prefix)
+(defun aod.tab-bar/make-project-tab-bar-post-hook (prefix)
+  
   (lambda ()
     "Hook to move buffer to its own tab, named by `prefix: <current-project-name>`"
     (when (project-current)
@@ -17,6 +18,19 @@
 	    ;; embark & xref seems to mess up with the current buffer
 	    (switch-to-buffer-other-window buffer)
 	    (delete-other-windows)))))))
+
+(defun aod.tab-bar/make-project-tab-bar-pre-hook (prefix)
+  (lambda ()
+    (when (project-current)
+      "Hook to move buffer to its own tab, named by `prefix: <current-project-name>`"
+      (let ((is-explicit-named (alist-get 'explicit-name (tab-bar--current-tab)))
+	    (current-name (alist-get 'name (tab-bar--current-tab)))
+	    (wanted-name (format "%s%s" prefix
+				 (project-name (project-current)))))
+	(when (and is-explicit-named
+		   (not (string= current-name wanted-name)))
+	  (message "switching to tab %s" wanted-name)
+	  (tab-bar-switch-to-tab wanted-name))))))
 
 (provide 'aod-tab-bar-hooks)
 
