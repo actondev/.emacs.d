79a00cc39a961126fee4055b90d4a2ed7d893e22
actondev <chris.actondev@gmail.com>
2023-03-25 01:31:47 +0100

    using elpaca

--- init.el
+++ init.el
@@ -1,39 +1,79 @@
 (defmacro comment (&rest a))
-(setq start-time (float-time))
 
-(defun msg-time (text)
-  (message (format "%s : %f" text (- (float-time) start-time))))
-
-;; WIP: startup time
-(setq gc-cons-threshold 64000000
+;; Make gc pauses faster by decreasing the threshold.
+;; we later lower it
+(setq gc-cons-threshold (* 64 1000 1000)
       inhibit-startup-screen t)
 
-(add-hook 'after-init-hook
-	  #'(lambda ()
-	      (setq gc-cons-threshold 800000)))
-;; startup time end
+(setq native-comp-async-report-warnings-errors nil)
 
-
-;; to be able to load with custom init.el location
-;; see https://emacs.stackexchange.com/questions/4253/how-to-start-emacs-with-a-custom-user-emacs-directory
-(setq user-init-file (or load-file-name (buffer-file-name)))
-;; (setq user-emacs-directory (file-name-directory user-init-file))
-
-(defun relative-from-init (file)
-  "Returns a complete file path relative from the init file.
-  eg if loaded ~/.emacs.d/init.el and you pass config.org you get
-  ~/.emacs.d/config.org"
-  (format "%s%s" (file-name-directory user-init-file) file))
-
-(setq custom-file (relative-from-init ".emacs-custom.el"))
+(setq custom-file (expand-file-name ".emacs-custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(load (relative-from-init "functions.el"))
+(if t
+    (progn
+      (load (expand-file-name "bootstrap/elpaca.el" user-emacs-directory)))
+  (progn
+    (load (expand-file-name "bootstrap/use-package.el" user-emacs-directory))))
 
-(package-initialize)
-;; loading org files : not loading org-mode everytime, but load the .el cached file
-(load-org-cached "config.org")
-(load-org-cached "elisp.org")
+(setq use-package-always-ensure t
+      use-package-always-defer t
+      use-package-verbose 'verbose
+      use-package-minimum-reported-time 0.01)
 
-(message (format "%s%s" "Startup time: " (emacs-init-time)))
+;; for load-org-cached
+(load (expand-file-name "functions.el" user-emacs-directory))
+
+(message "Bootstrapped functions & package manager in %.2f seconds"
+	 (float-time (time-subtract (current-time) before-init-time)))
+
+(let ((start-time (current-time)))
+  (load-org-cached "config.org")
+  (load-org-cached "elisp.org")
+  (message "Loaded .org files in %.2f seconds"
+	   (float-time (time-subtract (current-time) start-time))))
+
+(defun aod/require-init-packages ()
+  (message "Requiring init packages...")
+  (let ((start-time (current-time))
+	(packages '(
+		    minions
+		    doom-themes
+		    doom-modeline
+		    dashboard
+		    company
+		    which-key
+		    vertico
+		    ;; from elisp/
+		    ;; TODO autoloads not present?
+		    ;; that's why I need to require those
+		    aod-do
+		    pin-mode
+		    nice-jumper
+		    )))
+    (dolist (package packages)
+      (condition-case err
+	  (require package)
+	(error (warn "Error while requiring %s: %s" package (error-message-string err)))))
+    (message "   ...requiring init packages took %.2f seconds"
+	     (float-time (time-subtract (current-time) start-time)))))
+
+(setq aod/package-after-init-hook
+      (if (featurep 'elpaca)
+	  'elpaca-after-init-hook
+	'after-init-hook))
+
+;; revert gc threshold (should use after-init-hook or elpaca-after-init-hook if using elpaca)
+(add-hook aod/package-after-init-hook
+	  #'(lambda ()
+	      (setq gc-cons-threshold (* 2 1000 1000))))
+
+(add-hook aod/package-after-init-hook
+          (lambda ()
+            (message "Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time (time-subtract (current-time) before-init-time)))
+                     gcs-done)
+	    (aod/require-init-packages)
+	    ))
