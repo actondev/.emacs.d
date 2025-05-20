875540f3d2bb590864ad3087112952448d809623
actondev <chris.actondev@gmail.com>
2025-05-20 19:06:49 +0200

    aod-logos.el

---
+++ elisp/aod-logos.el
@@ -0,0 +1,47 @@
+;;; aod-logs.el --- simple presentation mode for org file  -*- lexical-binding: t; -*-
+
+(setq aod-logos/present-mode-map (make-sparse-keymap))
+;; Note: when using aod.logos/present-mode I had errors in emacs < 29
+;; during easy-mmode--mode-docstring
+;; called from define-minor-mode
+(define-minor-mode aod-logos/present-mode
+  "Toggle present mode"
+  :init-value nil
+  :lighter "present"
+  :keymap aod-logos/present-mode-map
+  (require 'logos)
+  (if aod-logos/present-mode
+      (progn
+	(logos-narrow-dwim)
+	(logos-focus-mode 1)
+	(logos--mode 'menu-bar-mode -1)
+	(logos--mode 'tool-bar-mode -1)
+	(logos--mode 'display-line-numbers-mode -1)
+	(logos--mode 'org-modern-mode 1)
+	(setq-local aod.log/org-hide-emphasis-markers org-hide-emphasis-markers)
+	(setq-local org-hide-emphasis-markers t)
+	;; trigger update for org-hide-emphasis-markers
+	(font-lock-mode 1)
+	)
+    (progn
+      (logos-focus-mode -1)
+      (setq-local org-hide-emphasis-markers aod.log/org-hide-emphasis-markers)
+      (widen))))
+
+(define-key aod-logos/present-mode-map (kbd "<f7>") 'logos-backward-page-dwim)
+(define-key aod-logos/present-mode-map (kbd "<f9>") 'logos-forward-page-dwim)
+(define-key aod-logos/present-mode-map (kbd "M-p") 'logos-backward-page-dwim)
+(define-key aod-logos/present-mode-map (kbd "M-n") 'logos-forward-page-dwim)
+
+(defun aod-logos/backward-skip-comment (&rest args)
+  (when (org-in-commented-heading-p)
+    (call-interactively #'logos-backward-page-dwim)))
+
+(defun aod-logos/forward-skip-comment (&rest args)
+  (when (org-in-commented-heading-p)
+    (call-interactively #'logos-forward-page-dwim)))
+
+(advice-add 'logos-backward-page-dwim :after #'aod-logos/backward-skip-comment)
+(advice-add 'logos-forward-page-dwim :after #'aod-logos/forward-skip-comment)
+
+(provide 'aod-logos)
